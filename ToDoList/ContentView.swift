57f8f31ded0ec21c8e1cb517fa6fb3a8e819796e
Kriti Sunkara <karasunshine0@gmail.com>
2025-07-14 10:47:50 -0400

    added the bindable property wrapper

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -42,7 +42,7 @@ struct ContentView: View {
         }
         
         if showNewTask {
-            NewToDoView()
+            NewToDoView(toDoItem: ToDoItem(title: "", isImportant: false))
         }
 
     }
